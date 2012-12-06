c7716911c889a6deed4af0f3e55f56db6c76b99a
Mercè Martín <mmartin@atinet.es>
2012-12-06 02:52:29 +0100

    Changes to docs, locale and date formatting
    - separates README.rst and docs/index.rst
    - retrieves locale also from dataset and model
    - solves date format problem in windows

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ distutils.core.setup(
     setup_requires = [],
     packages = ['bigmler'],
     include_package_data = True,
-    install_requires = ['bigml==0.4.5'],
+    install_requires = ['bigml==0.4.6'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
