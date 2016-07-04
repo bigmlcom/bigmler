f32c65b97208316495a694542592cdd0fbf02635
Mercè Martín Prats <merce@bigml.com>
2016-07-04 23:55:53 +0200

    Fixing bug: adding the whizzml module to the setup

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setuptools.setup(
                 'bigmler.options', 'bigmler.delete', 'bigmler.sample',
                 'bigmler.tests', 'bigmler.reify', 'bigmler.project',
                 'bigmler.association', 'bigmler.logisticregression',
-                'bigmler.execute'],
+                'bigmler.execute', 'bigmler.whizzml'],
     install_requires = ['bigml>=4.6.3, <4.7.0'],
     package_data={'bigmler':['static/*.json', 'static/*.html']},
     classifiers=[
