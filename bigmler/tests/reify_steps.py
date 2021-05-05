481a0b55e514dcae00106ff52fa1af8b4bbe7bef
Mercè Martín Prats <merce@bigml.com>
2021-05-05 12:51:53 +0200

    Changing test to avoid failing because of temporal ids

--- bigmler/tests/reify_steps.py
+++ bigmler/tests/reify_steps.py
@@ -104,6 +104,12 @@ def i_check_output_file(step, output=None, check_file=None):
     check_contents = re.sub(p_str,
                             '', check_contents,
                             flags=re.S)
+    #strip ids added in jupyter notebook transformation
+    # e.g.: ,\n"id": "3185730b"
+    p_str = r',"id": ".{8}"'
+    output_file_contents = re.sub(p_str,
+                                  '', output_file_contents,
+                                  flags=re.S)
     p_str = r';;.*\n'
     output_file_contents = re.sub(p_str,
                                   '', output_file_contents,
