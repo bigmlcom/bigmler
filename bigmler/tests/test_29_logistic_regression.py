e461b89ec10e33702f80f2dbf8601a7d9ff0251e
Mercè Martín Prats <merce@bigml.com>
2023-01-10 21:06:12 +0100

    Changing copyright notice

--- bigmler/tests/test_29_logistic_regression.py
+++ bigmler/tests/test_29_logistic_regression.py
@@ -2,7 +2,7 @@
 #!/usr/bin/env python
 #pylint: disable=locally-disabled,line-too-long,attribute-defined-outside-init
 #
-# Copyright 2016-2022 BigML
+# Copyright 2016-2023 BigML
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
