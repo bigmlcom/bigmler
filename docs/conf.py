5add165a4ef02bdf4336145d498c345ea84c9ca0
Mercè Martín Prats <merce@bigml.com>
2024-06-13 23:12:52 +0200

    Adding docs/requirements.txt for readthedocs [skip actions]

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ sys.path.insert(0, project_path)
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
-    'sphinx_rtd_theme'
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
